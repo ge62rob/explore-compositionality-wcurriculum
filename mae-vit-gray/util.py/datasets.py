0f1b3553bf8d70ecf2e1e90edac093c14f02db2f
ge62rob <emre.demirci@tum.de>
2025-07-05 02:49:00 +0200

    Add files via upload

---
+++ mae-vit-gray/util.py/datasets.py
@@ -0,0 +1,106 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# --------------------------------------------------------
+
+import os
+import PIL
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets, transforms
+from PIL import Image
+
+from timm.data import create_transform
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
+
+def build_dataset(is_train, args):
+    transform = build_transform(is_train, args)
+
+    root = os.path.join(args.data_path, 'train' if is_train else 'val')
+    dataset = TangramDataset(root, transform=transform)
+
+    print(dataset)
+
+    return dataset
+
+
+def build_transform(is_train, args):
+    mean = IMAGENET_DEFAULT_MEAN
+    std = IMAGENET_DEFAULT_STD
+    # train transform
+    if is_train:
+        # this should always dispatch to transforms_imagenet_train
+        transform = create_transform(
+            input_size=args.input_size,
+            is_training=True,
+            color_jitter=args.color_jitter,
+            auto_augment=args.aa,
+            interpolation='bicubic',
+            re_prob=args.reprob,
+            re_mode=args.remode,
+            re_count=args.recount,
+            mean=mean,
+            std=std,
+        )
+        return transform
+
+    # eval transform
+    t = []
+    if args.input_size <= 224:
+        crop_pct = 224 / 256
+    else:
+        crop_pct = 1.0
+    size = int(args.input_size / crop_pct)
+    t.append(
+        transforms.Resize(size, interpolation=PIL.Image.BICUBIC),  # to maintain same ratio w.r.t. 224 images
+    )
+    t.append(transforms.CenterCrop(args.input_size))
+
+    t.append(transforms.ToTensor())
+    t.append(transforms.Normalize(mean, std))
+    return transforms.Compose(t)
+
+
+class TangramDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_paths = []
+        
+        # Look for images in all subdirectories
+        print(f"Looking for images in: {root_dir}")
+        
+        # Check if directory exists
+        if not os.path.exists(root_dir):
+            print(f"WARNING: Directory not found at {root_dir}")
+            return
+            
+        # Find all image files recursively in all class subdirectories
+        for subdir, _, files in os.walk(root_dir):
+            for file in files:
+                if file.endswith(('.jpg', '.jpeg', '.png')):
+                    self.image_paths.append(os.path.join(subdir, file))
+        
+        print(f"Found {len(self.image_paths)} images")
+        
+    def __len__(self):
+        return len(self.image_paths)
+        
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        
+        # Load image
+        image = Image.open(img_path).convert('RGB')
+        
+        # Apply transforms
+        if self.transform:
+            image = self.transform(image)
+            
+        # For training, we don't need labels
+        return image, 0  # 0 is a dummy label
